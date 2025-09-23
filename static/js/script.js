acff51ae588774dd7e90047c9043a7c35d4ab7e8
JuanBlancodev <juanblancodev@gmail.com>
2025-09-23 08:18:13 -0700

    Primer commit para el repositorio del menu del 4-2

---
+++ static/js/script.js
@@ -0,0 +1,46 @@
+function mostrarTareas(mes) {
+  const tareas = {
+    septiembre: [
+      { texto: 'HTML - CSS - JS -> AUDIO', url: 'https://placehold.co/600x400?text=Ejercicio: Audio' },
+      { texto: 'HTML - CSS - JS -> VIDEO', url: 'https://placehold.co/600x400?text=Ejercicio: Video' },
+    ],
+    octubre: [
+      { texto: 'HTML - CSS - JS -> MULTIPLES IMÁGENES', url: 'https://placehold.co/600x400?text=Ejercicio: Multiples imágenes' },
+      { texto: 'HTML - CSS - JS -> COMBINACIÓN DE OBJETOS', url: 'https://placehold.co/600x400?text=Ejercicio: Combinación de objetos' },
+    ],
+    noviembre: [
+      { texto: 'HTML - CSS - JS -> RESPONSIVOS', url: 'https://placehold.co/600x400?text=Ejercicios: Responsivos' },
+      { texto: 'HTML - CSS - JS -> ANIMACIONES', url: 'https://placehold.co/600x400?text=Ejercicios: Animaciones' },
+    ],
+  }
+
+  const lista = document.getElementById("lista-tareas")
+  lista.innerHTML = ''
+  lista.classList.add("oculto")
+
+  tareas[mes].forEach(tarea => {
+    const li = document.createElement("li")
+    li.innerHTML = `<a href="${tarea.url}" target="_blank">${tarea.texto}</a>`
+    lista.appendChild(li)
+  })
+
+  lista.classList.remove("oculto")
+}
+
+function ocultarLista(){
+  const lista = document.getElementById("lista-tareas")
+  lista.classList.add("oculto")
+  lista.innerHTML = ""
+}
+
+function mostrarMensaje() {
+  const comentario = document.getElementById('comentario').value
+
+  if(comentario.trim() === ""){
+    alert('No has ingresado ningún texto')
+    return
+  }
+
+  alert(`Has enviado el comentario "${comentario}" al servidor.`)
+  document.getElementById('comentario').value = ""
+}
